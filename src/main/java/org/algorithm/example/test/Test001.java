6f55877fd8973105f0f8650394d67b9af3a47115
HideonBushz <2314656049@qq.com>
2024-07-09 14:12:27 +0800

    update_001

--- src/main/java/org/algorithm/example/test/Test001.java
+++ src/main/java/org/algorithm/example/test/Test001.java
@@ -2,6 +2,6 @@ package org.algorithm.example.test;
 
 public class Test001 {
     public static void main(String[] args) {
-        System.out.println("Test001");
+        System.out.println("Test00111");
     }
 }
--- src/main/java/org/algorithm/example/test/Test002.java
+++ src/main/java/org/algorithm/example/test/Test002.java
@@ -2,6 +2,6 @@ package org.algorithm.example.test;
 
 public class Test002 {
     public static void main(String[] args) {
-        System.out.println("Test002");
+        System.out.println("Test00222");
     }
 }
